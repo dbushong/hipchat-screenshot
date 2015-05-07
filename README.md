b275df9caf18e7145b20092727ca9073dd58c72d
David Bushong <david@bushong.net>
2015-05-07 09:03:44 -0700

    cleanup, document, and add save_dir support

--- README.md
+++ README.md
@@ -7,3 +7,12 @@ running Gnome desktop, and post it to your choice of a HipChat room.
 1. gem install hipchat
 1. put script wherever you want and run it once; it will create a skeletal `~/.hipchat-screenshot.yml` file, which you should edit with your list of rooms, API token, username, etc.
 1. run script; it will give you crosshairs to make a selection, then ask you whether/where to post it, then post it
+
+### Configuration ###
+
+In `~/.hipchat-screenshot.yml` you may configure:
+
+* `username`: required: your hipchat username
+* `api_token`: required: you can get it from [here](https://www.hipchat.com/account/api)
+* `rooms`: required: a hash of Room Name: Room Id   You can find the room ids [here](https://www.hipchat.com/rooms?t=mine) by looking at the links to the various rooms; the id is the number after `/show/` in the URL.
+* `save_dir`: optional: path to a directory where screenshots will be saved; default is to delete them after upload
--- hipchat-screenshot.rb
+++ hipchat-screenshot.rb
@@ -7,55 +7,65 @@ require 'rbconfig'
 require 'yaml'
 require 'open3'
 
-prog = File.basename($0)
+def die(msg)
+  $stderr.puts "#{Prog}: #{msg}"
+  exit 1
+end
+
+Prog = File.basename($0)
 opts = OptionParser.new
-opts.banner = "usage: #{prog} [options]"
+opts.banner = "usage: #{Prog} [options]"
 opts.on_tail('-h', '--help', 'Show this message') { puts opts ; exit 1 }
 begin
   opts.parse!(ARGV)
 rescue OptionParser::ParseError => e
-  $stderr.puts "#{prog}: #{e}\n#{opts}"
-  exit 1
+  die "#{e}\n#{opts}"
 end
 
-conf_file = "#{ENV['HOME']}/.hipchat-screenshot.yml"
+conf_file = File.join ENV['HOME'], '.hipchat-screenshot.yml'
 
 unless File.exists?(conf_file)
   skeleton = {
     'api_token' => 'FIXME',
     'rooms' => { 'FIXME Room Name' => 123456, 'FIXME Another' => 789012 },
-    'username' => 'FIXME: Your HipChat username',
+    'username' => 'FIXME: Your HipChat username'
   }
   File.open(conf_file, 'w') {|f| f.write skeleton.to_yaml }
   File.chmod(0600, conf_file)
-  $stderr.puts "Skeleton #{conf_file} created; edit then re-run"
-  exit(1)
+  die "skeleton #{conf_file} created; edit then re-run"
 end
 
 conf = YAML.load_file(conf_file)
 
-if conf['api_token'] == 'FIXME'
-  $stderr.puts "You need to EDIT #{conf_file} first"
-  exit(1)
+die "you need to EDIT #{conf_file} first" if conf['api_token'] == 'FIXME'
+
+# right now we assume mac or gnomeish desktop w/ zenity
+mac = !!(RbConfig::CONFIG['host_os'] =~ /darwin|mac os/)
+
+png = if (save_dir = conf['save_dir'])
+  die "no such directory #{save_dir}" unless Dir.exists? save_dir
+  File.join save_dir, Time.now.strftime('hcss-%Y-%m-%d-%H-%M-%S.png')
+else
+  tmp = Tempfile.new([Prog, '.png'])
+  tmp.close
+  tmp.path
 end
 
-mac  = !!(RbConfig::CONFIG['host_os'] =~ /darwin|mac os/)
-
-png = Tempfile.new([prog, '.png'])
-png.close
-message = "Screenshot from #{ENV['USER']} at #{Time.now}"
 room_name = room_id = nil
 
-def_room_name = conf['rooms'].first.first
-
+def_room_name   = conf['rooms'].first.first
 single_question = "Upload screenshot to #{def_room_name}?"
 multi_prompt    = 'Upload screenshot to which room?'
 
-room_name = if mac
-  require 'shellwords'
-  require 'open3'
+def run(*cmd)
+  out, st = Open3.capture2e(*cmd)
+  out.chomp!
+  die(out) unless st == 0
+  out
+end
 
-  system("screencapture -i #{png.path.shellescape} 2>/dev/null") || exit(1)
+room_name = if mac
+  run 'screencapture', '-i', png
 
   oscript = if conf['rooms'].size > 1 
     <<-EOF
@@ -73,20 +83,21 @@ room_name = if mac
     EOF
   end
 
+  # osascript seems to always exit with non-zero status (!?)
   out, = Open3.capture2e('osascript', stdin_data: oscript)
   out.chomp!
   out == 'button returned:OK' ? def_room_name : out
 else
-  # FIXME: should use something like this to determine if you have a prompter
+  # FIXME: should use something like this to determine if you have zenity
   # available: https://github.com/vertiginous/pik/blob/master/lib/pik/which.rb
   raise 'requires zenity' unless File.exists? '/usr/bin/zenity'
 
-  system('gnome-screenshot', '--area', '--file', png.path) || exit(1)
+  run 'gnome-screenshot', '--area', '--file', png
 
   if conf['rooms'].size > 1
-    out, = Open3.capture2('zenity', '--list', "--title=#{multi_prompt}",
-                          '--column', 'Room Name', *conf['rooms'].keys)
-    (out || '').gsub(/\|.*|\n/, '')
+    out, = Open3.capture2e('zenity', '--list', "--title=#{multi_prompt}",
+                           '--column', 'Room Name', *conf['rooms'].keys)
+    out.gsub(/\|.*|\n/, '')
   else
     system('zenity', '--question', '--text', single_question) && def_room_name
   end
@@ -96,4 +107,4 @@ room_id = conf['rooms'][room_name]
 exit(1) unless room_id
 
 client = HipChat::Client.new(conf['api_token'], api_version: 'v2')
-client[room_id].send_file(conf['username'], '', File.open(png.path))
+client[room_id].send_file(conf['username'], '', File.open(png))
